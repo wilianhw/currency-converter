a02c895073066b74605da3fe8bd272c1877199bf
Wilian <wilian.hw@hotmail.com>
2023-11-29 20:02:06 -0300

    Initial commit

---
+++ src/test/java/dev/wilian/currencyconverter/CurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wilian.currencyconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
